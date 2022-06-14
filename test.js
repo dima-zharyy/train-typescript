02b30083383e2b8a30321740ceb1fa7ee909a727
dima-zharyy <zharyydimka@gmail.com>
2022-06-14 21:16:09 +0300

    added-task-2

--- test.js
+++ test.js
@@ -1,9 +1,79 @@
-var btn = document.querySelector("button");
-var input1 = document.querySelector("#num1");
-var input2 = document.querySelector("#num2");
-function add(num1, num2) {
+"use strict";
+exports.__esModule = true;
+/////////////////////////// Task 1 ///////////////////////////
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+// button.addEventListener("click", function () {
+//   console.log(add(+input1.value, +input2.value));
+// });
+/////////////////////////// Task 2 ///////////////////////////
+var age;
+age = 50;
+var name;
+name = "Max";
+var toggle;
+toggle = true;
+var empty;
+empty = null;
+var notInitialize;
+notInitialize = undefined;
+var callback = function (a) {
+    return 100 + a;
+};
+var anything;
+anything = -20;
+anything = "Text";
+anything = {};
+var some;
+some = "Text";
+var str;
+if (typeof some === "string") {
+    str = some;
+}
+var person;
+person = ["Max", 21];
+var Load;
+(function (Load) {
+    Load[Load["LOADING"] = 0] = "LOADING";
+    Load[Load["READY"] = 1] = "READY";
+})(Load || (Load = {}));
+var page = {
+    load: Load.LOADING
+};
+if (page.load === Load.LOADING) {
+    console.log("Страница загружается");
+}
+if (page.load === Load.READY) {
+    console.log("Страница загружена");
+}
+var variable;
+var litteralVar;
+function showMessage(message) {
+    console.log(message);
+}
+function calc(num1, num2) {
     return num1 + num2;
 }
-btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value));
-});
+function customError() {
+    throw new Error("Error");
+}
+var page1 = {
+    title: "The awesome page",
+    likes: 100,
+    accounts: ["Max", "Anton", "Nikita"],
+    status: "open",
+    details: {
+        createAt: "2021-01-01",
+        updateAt: "2021-05-01"
+    }
+};
+var page2 = {
+    title: "Python or Js",
+    likes: 5,
+    accounts: ["Alex"],
+    status: "close"
+};
--- test.ts
+++ test.ts
@@ -1,11 +1,109 @@
-const btn = document.querySelector("button");
-const input1 = document.querySelector("#num1")! as HTMLInputElement;
-const input2 = document.querySelector("#num2")! as HTMLInputElement;
+export {};
 
-function add(num1: number, num2: number) {
+/////////////////////////// Task 1 ///////////////////////////
+
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
+
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+
+// button.addEventListener("click", function () {
+//   console.log(add(+input1.value, +input2.value));
+// });
+/////////////////////////// Task 2 ///////////////////////////
+
+let age: number;
+age = 50;
+let name: string;
+name = "Max";
+let toggle: boolean;
+toggle = true;
+let empty: null;
+empty = null;
+let notInitialize: undefined;
+notInitialize = undefined;
+let callback = (a: number): number => {
+  return 100 + a;
+};
+
+let anything: any;
+anything = -20;
+anything = "Text";
+anything = {};
+
+let some: unknown;
+some = "Text";
+
+let str: string;
+
+if (typeof some === "string") {
+  str = some;
+}
+
+let person: [string, number];
+
+person = ["Max", 21];
+
+enum Load {
+  LOADING,
+  READY,
+}
+
+const page = {
+  load: Load.LOADING,
+};
+
+if (page.load === Load.LOADING) {
+  console.log("Страница загружается");
+}
+if (page.load === Load.READY) {
+  console.log("Страница загружена");
+}
+
+let variable: string | number;
+
+let litteralVar: "enable" | "disable";
+
+function showMessage(message: string): void {
+  console.log(message);
+}
+
+function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
 
-btn?.addEventListener("click", function () {
-  console.log(add(+input1.value, +input2.value));
-});
+function customError(): never {
+  throw new Error("Error");
+}
+
+type CustomType = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: {
+    createAt: string;
+    updateAt: string;
+  };
+};
+
+const page1: CustomType = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: "2021-01-01",
+    updateAt: "2021-05-01",
+  },
+};
+
+const page2: CustomType = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
